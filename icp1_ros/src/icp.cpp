37655a8903372dcdeb1815ae87133d25bfb83f36
panhachet <chetsokhpanha@gmail.com>
2024-11-30 21:12:13 +0100

    working icp1_ros

--- icp1_ros/src/icp.cpp
+++ icp1_ros/src/icp.cpp
@@ -1,181 +1,7 @@
-// #ifndef ICP_HPP
-// #define ICP_HPP
-
-// #include <vector>
-// #include <tuple>
-// #include <cmath>
-// #include <Eigen/Dense>
-// #include <limits>
-// #include <iostream>
-
-// using namespace Eigen;
-// using namespace std;
-
-// class icp
-// {
-// public:
-//     icp(const vector<vector<double>>& P_data, 
-//         const vector<vector<double>>& Q_data, 
-//         vector<double> x_init, 
-//         int iteration, 
-//         double tolerance)
-//         : P(P_data), Q(Q_data), X(x_init), iter(iteration), tol(tolerance) {}
-
-//     vector<vector<double>> combine_data(const vector<double>& data_x, const vector<double>& data_y)
-//     {
-//         vector<vector<double>> data;
-//         for (size_t i = 0; i < data_x.size(); i++)
-//         {
-//             data.push_back({data_x[i], data_y[i]});
-//         }
-//         return data;
-//     }
-
-//     vector<pair<int, int>> correspondence(const vector<vector<double>>& P, const vector<vector<double>>& Q)
-//     {
-//         int p_size = P.size();
-//         int q_size = Q.size();
-//         vector<pair<int, int>> correspondences;
-//         for (int i = 0; i < p_size; i++)
-//         {
-//             vector<double> p_point = {P[i][0], P[i][1]};
-//             double min_dist = numeric_limits<double>::infinity();
-//             int chosen_idx = -1;
-//             for (int j = 0; j < q_size; j++)
-//             {
-//                 double sum = 0;
-//                 vector<double> q_point = {Q[j][0], Q[j][1]};
-//                 for (size_t k = 0; k < p_point.size(); k++)
-//                 {
-//                     double diff = p_point[k] - q_point[k];
-//                     sum += diff * diff;
-//                 }
-//                 double dist = sqrt(sum);
-//                 if (dist < min_dist)
-//                 {
-//                     min_dist = dist;
-//                     chosen_idx = j;
-//                 }
-//             }
-//             correspondences.push_back({i, chosen_idx});
-//         }
-//         return correspondences;
-//     }
-
-//     MatrixXd rotation(const float& theta) const
-//     {
-//         MatrixXd rot(2, 2);
-//         rot << cos(theta), -sin(theta),
-//                sin(theta), cos(theta);
-//         return rot;
-//     }
-
-//     MatrixXd d_rotation(const float& theta) const
-//     {
-//         MatrixXd d_rot(2, 2);
-//         d_rot << -sin(theta), -cos(theta),
-//                 cos(theta), -sin(theta);
-//         return d_rot;
-//     }
-
-//     MatrixXd jacobian(const vector<double>& x, const vector<double>& p) const
-//     {
-//         Vector2d p_point(p[0], p[1]);
-//         float theta = x[2];
-//         MatrixXd J = MatrixXd::Zero(2, 3);
-//         J.block<2, 2>(0, 0) = Matrix2d::Identity();
-//         J.block<2, 1>(0, 2) = d_rotation(theta) * p_point;
-//         return J;
-//     }
-
-//     MatrixXd error(const vector<double>& x, const vector<double>& p, const vector<double>& q) const
-//     {
-//         Vector2d p_point(p[0], p[1]);
-//         Vector2d q_point(q[0], q[1]);
-//         MatrixXd R = rotation(x[2]);
-//         Vector2d t(x[0], x[1]);
-//         return (R * p_point + t - q_point);
-//     }
-
-//     tuple<MatrixXd, Vector3d> solver(const vector<double>& x, const vector<vector<double>>& P, 
-//                                      const vector<vector<double>>& Q, vector<pair<int, int>> correspondences)
-//     {
-//         MatrixXd H = MatrixXd::Zero(3, 3);
-//         Vector3d g = Vector3d::Zero(3);
-//         for (const auto& index : correspondences)
-//         {
-//             int i = index.first;
-//             int j = index.second;
-//             vector<double> p = P[i];
-//             vector<double> q = Q[j];
-//             MatrixXd e = error(x, p, q);
-//             MatrixXd J = jacobian(x, p);
-//             H += J.transpose() * J;
-//             g += J.transpose() * e;
-//         }
-//         return make_tuple(H, g);
-//     }
-
-//     tuple<vector<vector<double>>, vector<double>, bool> icp_function()
-//     {
-//         vector<double> x = X;
-//         vector<vector<double>> P_prev = P;
-//         vector<vector<double>> x_new = {X};
-//         vector<vector<double>> P_new;
-//         vector<double> Dx;
-//         for (int i = 0; i < iter; i++)
-//         {
-//             cout << i << endl;
-//             MatrixXd R = rotation(x[2]);
-//             Vector2d t(x[0], x[1]);
-//             vector<pair<int, int>> corr = correspondence(P_prev, Q);
-//             auto [H, g] = solver(x, P, Q, corr);
-//             Vector3d dx = -H.inverse() * g;
-//             Dx.push_back(dx.norm());
-//             for (int i = 0; i < 3; i++) {
-//                 x[i] += dx(i);
-//             }
-//             x[2] = atan2(sin(x[2]), cos(x[2])); 
-//             x_new.push_back(x);
-//             for (size_t i = 0; i < P.size(); i++)
-//             {
-//                 Vector2d rotated_point = R * Vector2d(P[i][0], P[i][1]) + t;
-//                 P_prev[i] = {rotated_point(0), rotated_point(1)};
-//             }
-//             P_new = P_prev;
-//             if (dx.norm() < tol)
-//             {
-//                 return make_tuple(P_new, Dx, true);
-//             }
-//         }
-//         return make_tuple(P_new, Dx, false);
-//     }
-
-//     void set_P(const vector<vector<double>>& new_P) {
-//         P = new_P;
-//     }
-
-//     void set_Q(const vector<vector<double>>& new_Q) {
-//         Q = new_Q;
-//     }
-
-//     void set_X(const vector<double>& new_x)
-//     {
-//         X = new_x;
-//     }
-
-// private:
-//     vector<vector<double>> P;  
-//     vector<vector<double>> Q;  
-//     vector<double> X;          
-//     int iter;                  
-//     double tol;                
-// };
- 
-// #endif  // ICP_HPP
-
-
 #include "icp.hpp"
+#include <unordered_map>
+#include <limits>
+#include <cmath>
 
 icp::icp(const vector<vector<double>>& P_data, 
             const vector<vector<double>>& Q_data, 
@@ -195,34 +21,75 @@ vector<vector<double>> icp::combine_data(const vector<double>& data_x, const vec
     return data;
 }
 
+// vector<pair<int, int>> icp::correspondence(const vector<vector<double>>& P, const vector<vector<double>>& Q)
+// {
+//     int p_size = P.size();
+//     int q_size = Q.size();
+//     vector<pair<int, int>> correspondences;
+//     for (int i = 0; i<p_size; i++)
+//     {
+//         vector<double> p_point  = {P[i][0], P[i][1]};
+//         double min_dist = numeric_limits<double>::infinity();
+//         int chosen_idx = -1;
+//         for (int j = 0; j<q_size; j++)
+//         {
+//             double sum = 0;
+//             vector<double> q_point  = {Q[j][0], Q[j][1]};
+//             for (size_t k = 0; k<p_point.size(); k++)
+//             {
+//                 double diff = p_point[k] - q_point[k];
+//                 sum += diff * diff;
+//             }
+//             double dist = sqrt(sum);
+//             if (dist < min_dist)
+//             {
+//                 min_dist =  dist;
+//                 chosen_idx =  j;
+//             }
+//         }
+//         correspondences.push_back({i, chosen_idx});
+//     }
+//     return correspondences;
+// }
+
 vector<pair<int, int>> icp::correspondence(const vector<vector<double>>& P, const vector<vector<double>>& Q)
 {
     int p_size = P.size();
     int q_size = Q.size();
     vector<pair<int, int>> correspondences;
-    for (int i = 0; i<p_size; i++)
-    {
-        vector<double> p_point  = {P[i][0], P[i][1]};
+
+    vector<double> Q_squared(q_size, 0);
+    for (int j = 0; j < q_size; j++) {
+        double sum = 0;
+        for (size_t k = 0; k < Q[j].size(); k++) {
+            sum += Q[j][k] * Q[j][k]; 
+        }
+        Q_squared[j] = sum;
+    }
+
+    for (int i = 0; i < p_size; i++) {
         double min_dist = numeric_limits<double>::infinity();
         int chosen_idx = -1;
-        for (int j = 0; j<q_size; j++)
-        {
+        double p_squared = 0;
+        for (size_t k = 0; k < P[i].size(); k++) {
+            p_squared += P[i][k] * P[i][k]; 
+        }
+
+        for (int j = 0; j < q_size; j++) {
             double sum = 0;
-            vector<double> q_point  = {Q[j][0], Q[j][1]};
-            for (size_t k = 0; k<p_point.size(); k++)
-            {
-                double diff = p_point[k] - q_point[k];
+            for (size_t k = 0; k < P[i].size(); k++) {
+                double diff = P[i][k] - Q[j][k];
                 sum += diff * diff;
             }
-            double dist = sqrt(sum);
-            if (dist < min_dist)
-            {
-                min_dist =  dist;
-                chosen_idx =  j;
+            if (sum < min_dist) {
+                min_dist = sum;
+                chosen_idx = j;
             }
         }
+
         correspondences.push_back({i, chosen_idx});
     }
+
     return correspondences;
 }
 
@@ -281,6 +148,19 @@ const vector<vector<double>>& Q, vector<pair<int, int>>  correspondences)
     }
     return make_tuple(H, g);
 }
+
+void icp::set_P(const vector<vector<double>>& new_P) {
+    P = new_P;
+}
+
+void icp::set_Q(const vector<vector<double>>& new_Q) {
+    Q = new_Q;
+}
+void icp::set_X(const vector<double>& new_x)
+{
+    X = new_x;
+}
+
 tuple<vector<vector<double>>, vector<double>, bool> icp::icp_function()
 {
     vector<double> x = X;
@@ -290,7 +170,6 @@ tuple<vector<vector<double>>, vector<double>, bool> icp::icp_function()
     vector<double> Dx;
     for (int i =0; i<iter; i++)
     {
-        cout << i << endl;
         MatrixXd R = rotation(x[2]);
         Vector2d t(x[0], x[1]);
         vector<pair<int, int>> corr = correspondence(P_prev,Q);
@@ -307,12 +186,11 @@ tuple<vector<vector<double>>, vector<double>, bool> icp::icp_function()
             Vector2d rotated_point = R * Vector2d(P[i][0], P[i][1]) + t;
             P_prev[i] = {rotated_point(0), rotated_point(1)};
         }
-    
         P_new = P_prev;
         if(dx.norm() < tol)
         {
-            
             return make_tuple(P_new, Dx, true);
+            break;
         }
     }    
     return make_tuple(P_new, Dx, false);
---
+++ icp1_ros/src/icp_main.cpp
@@ -0,0 +1,185 @@
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "std_msgs/msg/float32_multi_array.hpp"
+#include "visualization_msgs/msg/marker.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "tf2_ros/transform_broadcaster.h"
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2/LinearMath/Matrix3x3.h"
+
+#include "iostream"
+#include "vector"
+#include "icp.hpp"
+
+
+using namespace std;
+
+double round_to_two_decimal_places(double value)
+{
+    return std::round(value * 1000.0) / 1000.0;
+}
+
+void round_store_Q(vector<vector<double>>& store_Q) {
+    for (auto& point : store_Q) {
+        point[0] = round_to_two_decimal_places(point[0]);  // Round x
+        point[1] = round_to_two_decimal_places(point[1]);  // Round y
+    }
+}
+
+class icp_node : public rclcpp::Node 
+{
+    public:
+    icp_node(): Node("icp_node"), 
+    icp_(vector<vector<double>>{}, vector<vector<double>>{},
+    {0.0, 0.0, 0.0}, 1000, 1e-6)
+    {
+        rclcpp::QoS qos_profile(rclcpp::KeepLast(100));
+        qos_profile.reliability(rclcpp::ReliabilityPolicy::BestEffort);
+
+        laser_sub = this->create_subscription<sensor_msgs::msg::LaserScan>("scan", qos_profile,
+            std::bind(&icp_node::laser_callback, this, std::placeholders::_1)
+        );
+        odom_sub = this->create_subscription<nav_msgs::msg::Odometry>("odom", 10,
+            std::bind(&icp_node::odom_callback, this, std::placeholders::_1)
+        );
+
+        new_laser_pub = this->create_publisher<visualization_msgs::msg::Marker>("new_scan", 10);
+        timer = this->create_wall_timer(std::chrono::milliseconds(100), std::bind(&icp_node::timer_callback, this));
+    }
+
+    private:
+
+        void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
+        {
+            robot_pose[0]  = msg->pose.pose.position.x;
+            robot_pose[1] = msg->pose.pose.position.y;
+            auto q = msg->pose.pose.orientation;
+            robot_pose[2]  = std::atan2(2.0*(q.w*q.z + q.x*q.y), 1.0 - 2.0*(q.y*q.y  + q.z*q.z));
+            double dx = robot_pose[0] - prev_robot_pose[0];
+            double dy = robot_pose[1] - prev_robot_pose[1];
+            double dtheta = robot_pose[2] - prev_robot_pose[2];
+            double distance_moved = std::sqrt(dx * dx + dy * dy);
+            double angle_moved = std::fabs(dtheta);
+            if (distance_moved > 0.005 || angle_moved > 0.01)
+            {
+                robot_move = true;
+            }
+            else
+            {
+                robot_move = false;
+            }
+            prev_robot_pose = robot_pose;
+
+        }
+
+        void laser_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
+        {
+            obj_x.clear();
+            obj_y.clear();
+            for(size_t i =0; i<msg->ranges.size(); i++)
+            {
+                float range = msg->ranges[i];
+                if(range < msg->range_min || range > msg->range_max)
+                {
+                    continue;
+                }
+
+                float angle =  msg->angle_min + i * msg->angle_increment;
+                float x = range * cos(angle);
+                float y = range * sin(angle);
+                float obx = x * cos(robot_pose[2]) - y * sin(robot_pose[2]);
+                float oby = x * sin(robot_pose[2]) + y * cos(robot_pose[2]);
+                obj_x.push_back(x);
+                obj_y.push_back(y);
+            }
+            obj =  icp_.combine_data(obj_x, obj_y);
+        }
+
+        void timer_callback()
+        {
+            if(!robot_move)
+            {
+                return;
+            }
+            if(flag == 0)
+            {
+                flag =  1;
+                if (Q.empty())
+                {
+                    Q =  obj;
+                    RCLCPP_WARN(this->get_logger(), "No points available for marker visualization.");
+                    flag = 0;
+                }
+                else
+                {
+                    P = obj;
+                    icp_.set_P(P);
+                    icp_.set_Q(Q);
+                    auto [aligned, dx, conv] = icp_.icp_function();
+                    cout << "conv" <<  conv << endl;
+                    cout << Q.size() << endl;
+                    if (conv)
+                    {
+                        icp_.set_X(dx);
+                        Q.insert(Q.end(), aligned.begin(), aligned.end());
+                        round_store_Q(Q);
+                        sort(Q.begin(), Q.end());
+                        Q.erase(unique(Q.begin(), Q.end()), Q.end());
+                        auto marker =  visualization_msgs::msg::Marker();
+                        marker.header.frame_id = "map";
+                        marker.header.stamp = this->get_clock()->now();
+                        marker.ns = "scan_points";
+                        marker.id = 0;
+                        marker.type = visualization_msgs::msg::Marker::SPHERE_LIST;
+                        marker.action = visualization_msgs::msg::Marker::ADD;
+                        marker.scale.x = 0.05;  
+                        marker.scale.y = 0.05;
+                        marker.scale.z = 0.05;
+                        marker.color.a = 1.0;  
+                        marker.color.r = 0.0;
+                        marker.color.g = 1.0;
+                        marker.color.b = 0.0;
+                        for (const  auto &point : Q)
+                        {
+                            geometry_msgs::msg::Point p;
+                            p.x = point[0];
+                            p.y = point[1];
+                            p.z = 0.0;
+                            marker.points.push_back(p);
+                        }
+                        new_laser_pub->publish(marker);
+                    }
+                    flag = 0;
+                }
+                robot_move = false;
+                }
+        }
+
+        rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_sub;
+        rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub;
+        
+        rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr new_laser_pub;
+        rclcpp::TimerBase::SharedPtr timer;
+        icp icp_;
+
+        vector<double> robot_pose = {0.0, 0.0, 0.0};
+        vector<double> prev_robot_pose = {0.0, 0.0, 0.0};
+        vector<vector<double>> obj;
+        vector<vector<double>> P;
+        vector<vector<double>> Q;
+        vector<double> obj_x;
+        vector<double> obj_y;
+        int flag = 0;
+        bool robot_move = false;
+
+
+};
+
+int main(int argc,  char **argv)
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<icp_node>());
+    rclcpp::shutdown();
+    return 0;
+}
